03aa1da2c105fec9eb2e3e8e88fbf4544e1de324
igads3d <igads3d@gmail.com>
2020-03-31 20:22:22 +0300

    set up an app

--- emptyproject/emptyproject/urls.py
+++ emptyproject/emptyproject/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from emptyapp import views
 
 urlpatterns = [
+	path("test/", views.test, name='test'),
     path('admin/', admin.site.urls),
 ]
