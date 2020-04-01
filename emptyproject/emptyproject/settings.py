cd76c41ddb6aaa3eeb9f4742bcb75447ed71b9ca
igads3d <igads3d@gmail.com>
2020-04-02 00:02:18 +0300

    structure and logic fix

--- emptyproject/emptyproject/settings.py
+++ emptyproject/emptyproject/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'emptyproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'emptyproject-db',
+        'USER': 'emptyproject-user'.
+        'PASSWORD': 'asshole',
+        'HOST': 'database',
+        'PORT': '5432',
     }
 }
 
@@ -119,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
